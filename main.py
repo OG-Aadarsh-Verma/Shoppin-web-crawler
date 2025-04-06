5e60bca3d6b52e6afee6238a80b6069a555942c8
OG-Aadarsh-Verma <aadarsh.av.verma@gmail.com>
2025-04-06 16:42:30 +0530

    Added shutdown failsafe

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from project.core.crawler import Crawler
 from project.database.domain_mapper import DomainMapper
 import asyncio
+import os
 
 def main():
+    try:
+        # Fail-safe in case shutdown.signal was not removed previously.
+        os.remove("shutdown.signal")
+    except Exception:
+        pass
+
     try:
         DomainMapper().save_domain()
         asyncio.run(Crawler().run_crawler())
@@ -11,7 +18,8 @@ def main():
     except KeyboardInterrupt:
         Crawler().shutdown()
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        print(f"An unexpected error occurred: {e}")        
+
 
 
 if __name__ == "__main__":
